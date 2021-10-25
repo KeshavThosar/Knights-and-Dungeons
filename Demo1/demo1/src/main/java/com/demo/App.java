1017d73b246d9e8b9e62bd4e953dc318dfd25312
Keshav Thosar <keshavjaythosar@gmail.com>
2021-10-25 20:00:35 +0530

    Initial Commit

---
+++ Demo1/demo1/src/main/java/com/demo/App.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import de.gurkenlabs.litiengine.Game;
+
+public class App {
+
+  public static void main(String[] args) {
+    Game.init(args);
+    Game.start();
+  }
+}
+
---
+++ LITIENGINE/utiLITI/utiliti.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\Java\jdk-14.0.1\bin\java" -jar utiliti-v0.5.1-beta.jar 
