0862b78327fe8e2b6947b4bf7dcbf2c57e840bbf
maitri-7 <maitriv7@gmail.com>
2021-10-25 20:32:03 +0530

    Second commit

--- Demo1/demo1/src/main/java/com/demo/App.java
+++ Demo1/demo1/src/main/java/com/demo/App.java
@@ -5,6 +5,7 @@ import de.gurkenlabs.litiengine.Game;
 public class App {
 
   public static void main(String[] args) {
+    System.out.println("heelloo");
     Game.init(args);
     Game.start();
   }
--- LITIENGINE/utiLITI/utiliti.bat
+++ LITIENGINE/utiLITI/utiliti.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files\Java\jdk-14.0.1\bin\java" -jar utiliti-v0.5.1-beta.jar 
+"C:\Program Files\Java\jdk-14.0.2\bin\java" -jar utiliti-v0.5.1-beta.jar 
